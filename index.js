50d646bb1a4afe3bd5cffeb7c7346d2c331378d8
Saliovin <jonas.eukan@gmail.com>
2022-05-05 20:38:56 +0800

    Added dark mode

--- index.js
+++ index.js
@@ -11,4 +11,28 @@ const observer = new IntersectionObserver(entries => {
   })
 })
 
+// eslint-disable-next-line no-unused-vars
+const toggleTheme = () => {
+  const bodyClassList = document.querySelector("body").classList
+  const toggleClassList = document.querySelector(".fa-solid").classList
+  if (bodyClassList.contains("dark-theme")) {
+    bodyClassList.remove("dark-theme")
+    toggleClassList.value = "fa-solid fa-moon"
+    document.querySelectorAll(".theme").forEach(document => {
+      const newSrc = document.getAttribute("src").split("/")
+      newSrc.splice(2,1)
+      document.setAttribute("src", newSrc.join("/"))
+    })
+  }
+  else {
+    bodyClassList.add("dark-theme")
+    toggleClassList.value = "fa-solid fa-sun"
+    document.querySelectorAll(".theme").forEach(document => {
+      const newSrc = document.getAttribute("src").split("/")
+      newSrc.splice(2,0,"dark-mode")
+      document.setAttribute("src", newSrc.join("/"))
+    })
+  }
+}
+
 window.onload = () => document.querySelectorAll(".anim").forEach(document => observer.observe(document))
